a18bae11cc4a18c108068fa80b355d316ca1a0f9
Madhav Sarpal <35966732+madhavsarpal100@users.noreply.github.com>
2018-12-08 17:05:21 +0530

    Add files via upload

---
+++ pred.r
@@ -0,0 +1,13 @@
+#dat<-read.csv("now.csv")
+
+# Give the chart file a name.
+#png(file = "line_chart.jpg")
+#AQI<-dat$aqi
+#gti<-c(250,255,270,257,280,310,330,335,340)
+#time<-dat$time
+# Plot the bar chart. 
+#plot(AQI,type = "o",col="red",xlab = "time",ylab="AQI",main="Past 9 Hours")
+
+# Save the file.
+#dev.off()
+
